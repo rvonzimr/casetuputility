508a955078509e2f48f7d7bc524ebb8c1d4fa78c
Robert Von Zimmerman <vonzimr@gmail.com>
2025-11-17 18:08:17 -0800

    readme and stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+100% Over-developed way to use the CA Setup utility.
+
+Patches chdir so the program doesn't try writing to a data folder alongside the binary.
+
+Tries to patch the QT theme so that the UI is usuable as well.
--- package.nix
+++ package.nix
@@ -17,11 +17,7 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-lRBhbNI8fnVW92se53dgR8y8ftUEGHM5XHfl55KvTmM=";
   };
 
-  patch_src = ./chdir_wrapper.c;
-  theme_patch_src = ./theme_patch.cpp;
-  light_theme = ./light-theme.qss;
-  wrapper_src = ./wrapper.sh;
-  forceQtStyle = "Breeze-light";
+  assets = ./assets;
 
   buildInputs = [
     qt5.qtbase
@@ -37,37 +33,28 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   preBuildPhase = ''
-    cp ${finalAttrs.patch_src} ./chdir_wrapper.c
-    cp ${finalAttrs.theme_patch_src} ./theme_patch.cpp
+    cp -r ${finalAttrs.assets}/* ./;
   '';
 
   buildPhase = ''
     runHook preBuildPhase
     # Compile our shared library. The output file will be in the current directory.
-    ${pkgs.gcc}/bin/gcc -fPIC -shared chdir_wrapper.c -o chdir_wrapper.so -ldl
-
-    # Compile the new theme patch (C++), linking against the necessary Qt libs
-    ${pkgs.gcc}/bin/g++ -fPIC -shared theme_patch.cpp -o theme_patch.so -ldl \
-      -I${qt5.qtbase.dev}/include -L${qt5.qtbase}/lib -lQt5Core -lQt5Gui -lQt5Widgets
+    ${pkgs.gcc}/bin/gcc -fPIC -shared chdir_wrapper.c -o ./chdir_wrapper.so -ldl
   '';
 
   installPhase = ''
     mkdir -p $out/bin $out/lib $out/share/CASetupUtility $out/libexec
 
     cp CASetupUtility $out/libexec/
-    cp chdir_wrapper.so $out/lib/
-    cp theme_patch.so $out/lib/
+    cp ./chdir_wrapper.so $out/lib/
     cp -r data $out/share/CASetupUtility/
-    cp ${finalAttrs.light_theme} $out/share/CASetupUtility/light-theme.qss
-
-    export qt_style="${finalAttrs.forceQtStyle}"
+    cp ./light-theme.qss $out/share/CASetupUtility/light-theme.qss
 
     patchelf \
       --add-needed chdir_wrapper.so \
-      --add-needed theme_patch.so \
       $out/libexec/CASetupUtility
 
-    substituteAll ${finalAttrs.wrapper_src} $out/bin/CASetupUtility
+    substituteAll ./wrapper.sh $out/bin/CASetupUtility
     chmod +x $out/bin/CASetupUtility
   '';
 
